3307b8260d3218ba88042c5accc2460b981f71aa
Sandra Fogg <sandrafogg@ESM.UCSB.EDU>
2019-10-31 13:08:41 -0700

    completed chapter 2 excercise 1

--- app.R
+++ app.R
@@ -1,9 +1,60 @@
-ui <- fluidPage(
-  "Hey, Bitches"
-)
+#ui <- fluidPage(
+ # selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
+#  verbatimTextOutput("summary"),
+#  tableOutput("table")
+#)
 
-server <- function(input, output, session) {
+#server <- function(input, output, session) {
+  #output$summary <- renderPrint({
+   # dataset <- get(input$dataset, "package:datasets")
+    # summary(dataset)
+  #})
   
-}
+  #output$table <- renderTable({
+   # dataset <- get(input$dataset, "package:datasets")
+    #dataset
+  #})
+#}
 
-shinyApp(ui, server)
+
+#In the above server function, the out put has duplicated code. Instead use reactive expressions so you only have to retrieive the data set once
+#Below is how you would correctly code with reactive expressions using the reactive ({})
+#server <- function(input, output, session) {
+ # dataset <- reactive({
+  #  get(input$dataset, "package:datasets")
+#  })
+  
+ # output$summary <- renderPrint({
+  #  summary(dataset())
+  #})
+  
+  #output$table <- renderTable({
+   # dataset()
+#  })
+#}
+
+
+#shinyApp(ui, server)
+
+
+#Chapter 2 Excercise 1
+
+#ui <- fluidPage(
+ # textInput("name", "What's your name?")
+
+#)
+
+#server <- function(input, output, session) {
+#output$summary <- renderText({
+ #sayhi <- paste0("Hello", input$name)
+#})
+
+#output$table <- renderTable({
+ # sayhi()
+#})
+#}
+
+#shinyApp(ui, server)
+
+
+
